e4e7b85e49f95bc80fc1665457ab6dcb5fea3a1c
rose-giant <itsrozasworld@gmail.com>
2024-02-23 22:30:42 -0500

    feat !: project was created Breaking change: maven dependencies were specified

---
+++ src/main/java/mizdooni/Main.java
@@ -0,0 +1,9 @@
+package mizdooni;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws JsonProcessingException {
+
+    }
+}
