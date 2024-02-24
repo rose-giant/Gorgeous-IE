ddd3af13f6458075d72488f1dd29287aa8c0421b
rose-giant <itsrozasworld@gmail.com>
2024-02-24 04:08:24 -0500

    fix: restaurant unit tests were added

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -46,4 +46,133 @@ public class RestaurantTest {
         date.setMinutes(10);
         assertFalse(restaurant.isTimeValid(date));
     }
+
+    @Test
+    public void handleNoneExistingUsernameFillsResponseHandler() {
+        restaurant.responseHandler.responseStatus = true;
+        restaurant.responseHandler.responseBody = "";
+
+        restaurant.handleNoneExistingUsername();
+        assertEquals(restaurant.responseHandler.responseBody, " manager username does not exist.");
+        assertFalse(restaurant.responseHandler.responseStatus);
+    }
+
+    @Test
+    public void handleNoneExistingUsernameUpdatesResponseHandler() {
+        restaurant.responseHandler.responseStatus = false;
+        restaurant.responseHandler.responseBody = "hello!";
+
+        restaurant.handleNoneExistingUsername();
+        assertEquals(restaurant.responseHandler.responseBody, "hello! manager username does not exist.");
+        assertFalse(restaurant.responseHandler.responseStatus);
+    }
+
+    @Test
+    public void handleIncorrectManagerRoleFillsResponseHandler() {
+        restaurant.responseHandler.responseStatus = true;
+        restaurant.responseHandler.responseBody = "";
+
+        restaurant.handleNoneExistingUsername();
+        assertEquals(restaurant.responseHandler.responseBody, " manager role is not correct.");
+        assertFalse(restaurant.responseHandler.responseStatus);
+    }
+
+    @Test
+    public void handleIncorrectManagerRoleUpdatesResponseHandler() {
+        restaurant.responseHandler.responseStatus = false;
+        restaurant.responseHandler.responseBody = "hello!";
+
+        restaurant.handleIncorrectManagerRole();
+        assertEquals(restaurant.responseHandler.responseBody, "hello! manager role is not correct.");
+        assertFalse(restaurant.responseHandler.responseStatus);
+    }
+
+    @Test
+    public void addRestaurantResponseGeneratorGeneratesSuccessfulResponse() {
+        restaurant.address.city = "milan";
+        restaurant.address.country = "italy";
+        restaurant.address.street = "alferedo";
+        restaurant.managerUsername = "Eunhoo";
+        restaurant.type = "italian";
+        restaurant.description = "yes";
+        restaurant.startTime = "11:0";
+        restaurant.endTime = "12:0";
+        restaurant.name = "vanila";
+
+        restaurant.addRestaurantResponseGenerator(restaurant);
+        assertTrue(restaurant.responseHandler.responseStatus);
+        assertEquals(restaurant.responseHandler.responseBody, "Restaurant added successfully.");
+    }
+
+    @Test
+    public void addRestaurantResponseGeneratorGeneratesResponseForInvalidTime() {
+        restaurant.responseHandler.responseBody = "";
+        restaurant.address.city = "milan";
+        restaurant.address.country = "italy";
+        restaurant.address.street = "alferedo";
+        restaurant.managerUsername = "me";
+        restaurant.type = "italian";
+        restaurant.description = "yes";
+        restaurant.startTime = "11:05";
+        restaurant.endTime = "12:30";
+        restaurant.name = "vanila";
+
+        restaurant.addRestaurantResponseGenerator(restaurant);
+        assertFalse(restaurant.responseHandler.responseStatus);
+        assertEquals( " start time is not rond. end time is not rond.", restaurant.responseHandler.responseBody);
+    }
+
+    @Test
+    public void addRestaurantResponseGeneratorGeneratesResponseForInvalidManagerUsername() {
+        restaurant.responseHandler.responseBody = "";
+        restaurant.address.city = "milan";
+        restaurant.address.country = "italy";
+        restaurant.address.street = "alferedo";
+        restaurant.managerUsername = "me*";
+        restaurant.type = "italian";
+        restaurant.description = "yes";
+        restaurant.startTime = "11:0";
+        restaurant.endTime = "12:0";
+        restaurant.name = "vanila";
+
+        restaurant.addRestaurantResponseGenerator(restaurant);
+        assertFalse(restaurant.responseHandler.responseStatus);
+        assertEquals( " manager username is not valid.", restaurant.responseHandler.responseBody);
+    }
+
+    @Test
+    public void addRestaurantResponseGeneratorGeneratesResponseForInvalidAddress() {
+        restaurant.responseHandler.responseBody = "";
+        restaurant.address.city = "milan";
+        restaurant.address.country = "";
+        restaurant.address.street = "alferedo";
+        restaurant.managerUsername = "me";
+        restaurant.type = "italian";
+        restaurant.description = "yes";
+        restaurant.startTime = "11:0";
+        restaurant.endTime = "12:0";
+        restaurant.name = "vanila";
+
+        restaurant.addRestaurantResponseGenerator(restaurant);
+        assertFalse(restaurant.responseHandler.responseStatus);
+        assertEquals( " address is not valid.", restaurant.responseHandler.responseBody);
+    }
+
+    @Test
+    public void addRestaurantResponseGeneratorGeneratesResponseForInvalidDescription() {
+        restaurant.responseHandler.responseBody = "";
+        restaurant.address.city = "milan";
+        restaurant.address.country = "italy";
+        restaurant.address.street = "alferedo";
+        restaurant.managerUsername = "me";
+        restaurant.type = "italian";
+        restaurant.description = "";
+        restaurant.startTime = "11:0";
+        restaurant.endTime = "12:0";
+        restaurant.name = "vanila";
+
+        restaurant.addRestaurantResponseGenerator(restaurant);
+        assertFalse(restaurant.responseHandler.responseStatus);
+        assertEquals( " description is not valid.", restaurant.responseHandler.responseBody);
+    }
 }
