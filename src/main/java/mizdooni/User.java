45a6b04b40ff0f807301769f88e680b7ba441e66
rose-giant <itsrozasworld@gmail.com>
2024-02-23 22:33:16 -0500

    feat : User validators were creted

--- src/main/java/mizdooni/User.java
+++ src/main/java/mizdooni/User.java
@@ -24,5 +24,53 @@ public class User {
     public ResponseHandler responseHandler;
     public String addUserResponse;
     public ArrayList<User> users = new ArrayList<User>();
+    public boolean isRoleValid(String role) {
+        validRoles.add(ROLE_1);
+        validRoles.add(ROLE_2);
+        if (validRoles.contains(role)) {
+            return true;
+        }  else {
+            return false;
+        }
+    }
+
+    public boolean isUsernameValid(String username) {
+        for (int i = 0 ; i < forbiddenCharacters.length ; i++) {
+            if (username.contains(forbiddenCharacters[i]+"")) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isEmailValid(String email) {
+        String[] adsSplit = email.split("@");
+
+        if (adsSplit.length != 2 ) {
+            return false;
+        }
+
+        if (!adsSplit[1].endsWith(".com")) {
+            return false;
+        }
+
+        String middleSplit = (String) adsSplit[1].subSequence(0, adsSplit[1].length() - 4);
+        if (middleSplit.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isPasswordValid(String password) {
+        return !password.isEmpty();
+    }
+
+    public boolean isAddressValid(Address address) {
+        boolean isCityValid = address.isCityValid(address.city);
+        boolean isCountryValid = address.isCountryValid(address.country);
+        return isCountryValid && isCityValid;
+    }
 
 }
