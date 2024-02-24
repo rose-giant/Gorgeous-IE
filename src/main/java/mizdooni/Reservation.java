235737e00d8e9419bba1acbb310aff97d26a84c2
Fatemeh <f.shahhosseini@gmail.com>
2024-02-25 02:24:51 +0330

    fix: Reservation class upgraded

--- src/main/java/mizdooni/Reservation.java
+++ src/main/java/mizdooni/Reservation.java
@@ -8,26 +8,30 @@ import java.util.ArrayList;
 //reserveReservation {“username”: “user1”, “restaurantName”: “restaurant1”, “reservationNumber”: 1,
 //        “datetime”: “2024-02-14 21:00”}
 public class Reservation {
+    public int reservationNumber;
     public String username;
 
     public String restaurantName;
 
-    public String reservationNumber;
+    public String tableNumber;
     
     public String datetime;
 
+    private static int counter;
+
     public Reservation(String jsonString) throws Exception {
+        counter += 1;
         ObjectMapper om = new ObjectMapper();
         Reservation reservation  = om.readValue(jsonString, Reservation.class);
         username = reservation.username;
         restaurantName = reservation.restaurantName;
-        reservationNumber = reservation.reservationNumber;
+        tableNumber = reservation.tableNumber;
         checkDateTimeFormat(reservation.datetime);
+        reservationNumber = counter;
     }
     public void checkDateTimeFormat(String datetime) throws Exception {
         if(datetime.matches("\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:00")){
             this.datetime = datetime;
         }else throw new Exception("DateTime format in not correct");
-
     }
 }
